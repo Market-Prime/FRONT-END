363420e21349ab50b032ea953be38736a6f58a11
olasubomiesther <olasubomiesther14@gmail.com>
2024-07-17 21:34:38 +0100

    Header and Footer Component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css'
+import Hero from './Header/Hero';
+import Footer from './Footer/Footer'
+function App() {
+    return(
+        <div>
+            <Hero />
+            <Footer />
+
+        </div>
+    )
+}
+
+export default App;
---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+const Footer = () =>{
+    return(
+        <div className="flex sm:flex-col md:flex-row lg:flex-row xl:flex-row 2xl:flex-row items-start justify-center w-full bg-blue-950 py-20 sm:py-10 md:px-10 xl:px-32 2xl:px-32 sm:px-10">
+            <div className="pr-70 mr-2 sm:mb-5 w-1/2">
+                <a className="block text-white mt-3 text-sm" href="#">About Us</a>
+                <a className="block text-white mt-3 text-sm" href="#">FAQs</a>
+                <a className="block text-white mt-3 text-sm" href="#">Contact Us</a>
+            </div>
+            <div className="pr-70 mr-2 sm:mb-5 w-1/2">
+                <a className="block text-white mt-3 text-sm" href="#">Terms of Use</a>
+                <a className="block text-white mt-3 text-sm" href="#">Privacy Policy</a>
+                <a className="block text-white mt-3 text-sm" href="#">Delivery Policy</a>
+            </div>
+            <div className="pr-70 mr-2 sm:mb-5 w-1/2">
+                <a className="block text-white mt-3 text-sm" href="#">LinkedIn</a>
+                <a className="block text-white mt-3 text-sm" href="#">Twitter(X)</a>
+                <a className="block text-white mt-3 text-sm" href="#">Instagram</a>
+            </div>
+            <div className="pr-70 mr-2 sm:mb-5 w-1/2">
+                <a className="block text-white mt-3 text-sm" href="#">Customer Service</a>
+                <a className="block text-white mt-3 text-sm" href="#">Newsletter</a>
+            </div>
+        </div>
+    )
+}
+export default Footer;
