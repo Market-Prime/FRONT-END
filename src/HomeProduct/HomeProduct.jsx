81f5b4e6c5a16b2b6c083c7b19c8d395595571f8
olasubomiesther <olasubomiesther14@gmail.com>
2024-07-23 16:09:05 +0100

    Changes on the component

--- src/HomeProduct/HomeProduct.jsx
+++ src/HomeProduct/HomeProduct.jsx
@@ -10,15 +10,15 @@ const HoomeProduct = () =>{
 
     const productList = product.map((item, i) =>{
         return(
-            <div key={i}>
+            <div key={i} className="md:mb-10 lg:mb-0 xl:mb-0 md:w-1/2 lg:w-1/3 xl:w-1/4">
                 <p className="text-center font-bold text-2xl">{item.name}</p>
                 <img className="text-center mx-auto" src={item.Image} />
             </div>
         )
     })
     return(
-        <div className="py-16 xl:px-40 lg:px-28">
-            <div className="flex items-center justify-between sm:flex-col xl:flex-row lg:flex-row md:flex-col">{productList}</div>
+        <div className="py-16 xl:px-40 lg:px-28 md:px-20">
+            <div className="flex items-center justify-between sm:flex-col xl:flex-row lg:flex-row md:flex-row flex-wrap">{productList}</div>
         </div>
     )
 }
