5159fc9faf64e964aa1e7a5c3199687febbf16ac
OLASUBOMI <olasubomiesther14@gmail.com>
2024-07-24 12:23:00 +0100

    Initial Commit

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const About = () =>{
     return (
         <div className="bg-aboutbg py-20 xl:px-40 lg:px-28 md:px-20 sm:px-10">
-            <h1 className="text-white xl:text-8xl lg:text-6xl md:text-5xl sm:text-4xl font-bold text-start tracking-widest">About Market Prime</h1>
+            <h1 className="text-white xl:text-7xl lg:text-6xl md:text-5xl sm:text-4xl font-bold text-start tracking-widest">About Market Prime</h1>
             <p className="mt-10 font-normal text-xl text-white text-justify">Market Prime is a cutting-edge fashion e-commerce platform dedicated to bringing the latest trends and high-quality fashion items to our customers. 
             <span  className="block mt-2">We offer a diverse range of products, including bags, shoes, shirts, trousers, and garments from renowned fashion brands.</span> 
             <span className="block mt-2">Our innovative platform utilizes advanced AI technology to provide a personalized shopping experience, allowing customers to visualize how items look on them through virtual try-ons before making a purchase.</span>
--- src/Header/Hero.jsx
+++ src/Header/Hero.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Hero = () =>{
     return(
         <div className="bg-herocolor px-10 py-10 xl:h-screen lg:h-screen md:h-screen sm:h-screen 2xl:h-screen  hero-section w-full">
-            <h1 className="font-bold lg:text-7xl md:text-5xl sm:text-4xl xl:text-8xl uppercase  text-white w-full xl:tracking-[.53em] lg:tracking-[.44em] sm:tracking-[.15em] md:tracking-[.32em] sm:mt-20 md:mt-32 lg:mt-10 xl:mt-10">market prime</h1>
+            <h1 className="font-bold lg:text-7xl md:text-5xl sm:text-4xl xl:text-7xl uppercase  text-white w-full xl:tracking-[.65em] lg:tracking-[.40em] sm:tracking-[.15em] md:tracking-[.32em] sm:mt-20 md:mt-32 lg:mt-10 xl:mt-10">market prime</h1>
             <div className="mt-10">
                 <p className="mt-10 text-white xl:w-1/3 lg:w-1/2 md:w-full sm:w-full text-justify sm:leading-9 md:leading-7 lg:leading-7 xl:leading-7">Revolutionize the online fashion shopping experience by combining technology and style. Provide our customers with an intuitive, engaging, and personalized shopping journey, ensuring they find the perfect items that suit their tastes and preferences.</p>
                 {/* <img src="/images/pexels-godisable-jacob-226636-784371 2.png" alt="Header Image" /> */}
