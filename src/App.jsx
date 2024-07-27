d393b669e4cb60ad3df78ea840c0a492f4606d38
OLASUBOMI <olasubomiesther14@gmail.com>
2024-07-27 15:13:51 +0100

    Changes made on the header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React , {useEffect} from 'react';
 import './App.css'
 import Hero from './Header/Hero';
 import About from './About/About';
 import HoomeProduct from './HomeProduct/HomeProduct';
 import Footer from './Footer/Footer'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 function App() {
+    useEffect(() => {
+        AOS.init({
+          duration:2000
+        });
+      }, [])
     return(
         <div>
             <Hero />
--- src/Header/Hero.jsx
+++ src/Header/Hero.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-
 const Hero = () =>{
-    return(
-        <div className="bg-herocolor px-10 py-10 xl:h-screen lg:h-screen md:h-screen sm:h-screen 2xl:h-screen  hero-section w-full">
-            <h1 className="font-bold lg:text-7xl md:text-5xl sm:text-4xl xl:text-7xl uppercase  text-white w-full xl:tracking-[.65em] lg:tracking-[.40em] sm:tracking-[.15em] md:tracking-[.32em] sm:mt-20 md:mt-32 lg:mt-10 xl:mt-10">market prime</h1>
-            <div className="mt-10">
-                <p className="mt-10 text-white xl:w-1/3 lg:w-1/2 md:w-full sm:w-full text-justify sm:leading-9 md:leading-7 lg:leading-7 xl:leading-7">Revolutionize the online fashion shopping experience by combining technology and style. Provide our customers with an intuitive, engaging, and personalized shopping journey, ensuring they find the perfect items that suit their tastes and preferences.</p>
-                {/* <img src="/images/pexels-godisable-jacob-226636-784371 2.png" alt="Header Image" /> */}
+    return (
+        <div className="main-heading h-screen sm:py-60 md:py-60  lg:py-52 xl:py-52 2xl:py-64 text-center">
+            <h1 className="text-white capitalize lg:text-5xl xl:text-5xl md:text-4xl sm:text-3xl font-extrabold italic" data-aos="fade-right" data-aos-duration="5000">smart shopping,stylish living</h1>
+            <p className="text-white my-5 lg:w-2/3 xl:w-2/3 md:w-full sm:w-full leading-loose lg:text-center xl:text-center md:text-center sm:text-justify mx-auto sm:px-4">Revolutionize the online fashion shopping experience by combining technology and style. Provide our customers with an intuitive, engaging, and personalized shopping journey, ensuring they find the perfect items that suit their tastes and preferences.</p>
+            <div className="flex lg:w-1/3  mx-auto rounded-2xl sm:mt-10 md:mt-o lg:mt-0 xl:mt-0 md:px-10">
+                <input type="text" placeholder="Search for your stylish product" className="w-full px-5 py-2 outline-none"/>
+                <button className="bg-aboutbg text-white py-2 px-5 w-1/2">Search</button>
             </div>
         </div>
     )
