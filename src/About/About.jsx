a0ba29cbef7a6af73335fc2bfcd1814a6e38f9bb
OLASUBOMI <olasubomiesther14@gmail.com>
2024-08-14 16:19:36 +0100

    Landing Page responsiveness

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="bg-aboutbg py-20 xl:px-40 lg:px-28 md:px-20 sm:px-10">
-      <h1 className="text-white xl:text-7xl lg:text-6xl md:text-5xl sm:text-4xl font-bold text-start tracking-widest">About Market Prime</h1>
+    <div className="bg-aboutbg xl:py-20 lg:py-20 md:py-20 sm:py-10 xl:px-40 lg:px-28 md:px-20 sm:px-10">
+      <h1 className="text-white xl:text-6xl lg:text-5xl md:text-5xl sm:text-xl font-bold text-start tracking-widest w-full">About Market Prime</h1>
       <p
-        className="mt-10 font-normal text-xl text-white text-justify"
+        className="xl:mt-10 lg:mt-10 md:mt-10 sm:mt-5 font-normal text-xl text-white text-justify"
         data-aos="zoom-in"
         data-aos-duration="1000"
       >Market Prime is a cutting-edge fashion e-commerce platform dedicated to bringing the latest trends and high-quality fashion items to our customers.</p>
